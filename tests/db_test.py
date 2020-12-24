f9763bc9e1238e2b779115b1094fdd4ae5b9e831
Bastien Boutonnet <bastien.b1@gmail.com>
2020-12-24 17:19:08 +0100

    remove pandas calls and update paths

--- tests/db_test.py
+++ tests/db_test.py
@@ -5,18 +5,12 @@ TEST_DIR = Path(__file__).resolve().parent
 
 
 def test_select(postgresql_db):
-    import pandas
-
     # this populates the db with stuff from the sql file.
     # We could also just run a conn.execute if we wanted to.
-    postgresql_db.run_sql_file(str(TEST_DIR.joinpath("db_setup.sql")))
+    postgresql_db.run_sql_file(str(TEST_DIR.joinpath("docker_postgres", "db_setup.sql")))
     with postgresql_db.engine.connect() as conn:
         r = conn.execute("SELECT * FROM test")
         results = list()
         for row in r:
             results.append(dict(row))
         print(results)
-        print(pandas.DataFrame.from_records(results))
-
-
-c = create_engine("postgresql://root:password@localhost/dbt_sugar")

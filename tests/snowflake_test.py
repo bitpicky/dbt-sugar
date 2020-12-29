f914ea91925802ba594f145c09be78dbb5603c32
Bastien Boutonnet <bastien.b1@gmail.com>
2020-12-29 12:53:54 +0100

    add tests for snowflake connector

---
+++ tests/snowflake_test.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+
+
+CREDENTIALS = dict(
+    user="dbt_sugar_test_user",
+    password="magical_password",
+    database="dbt_sugar",
+    account="dummy_account",
+)
+
+
+def test_generate_connection():
+    from dbt_sugar.core.connectors.snowflake_connector import SnowflakeConnector
+
+    conn = SnowflakeConnector(**CREDENTIALS).generate_connection()
+    assert isinstance(conn.engine, sqlalchemy.engine.Engine)

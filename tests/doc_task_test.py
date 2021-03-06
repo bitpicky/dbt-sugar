78ac1ed443b7bca69d99383ee639f9e6dbf20e83
Virginia Lopez-Gil <vlopezgil@tripactions.com>
2021-03-06 15:52:36 +0100

    Fixing tests

--- tests/doc_task_test.py
+++ tests/doc_task_test.py
@@ -136,7 +136,7 @@ def test_delete_failed_tests_from_schema(mocker, content, model_name, tests_to_d
     open_yaml = mocker.patch("dbt_sugar.core.task.doc.open_yaml")
     save_yaml = mocker.patch("dbt_sugar.core.task.doc.save_yaml")
     open_yaml.return_value = content
-    doc_task = DocumentationTask(None, None)
+    doc_task = DocumentationTask(None, None, None)
     doc_task.delete_failed_tests_from_schema(Path("."), model_name, tests_to_delete)
     save_yaml.assert_has_calls(result)
 

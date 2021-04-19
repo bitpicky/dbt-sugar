cdf3311d567dc969776910af7f8e7bfe6dd285ef
Bastien Boutonnet <bastien.b1@gmail.com>
2021-04-19 20:11:47 +0200

    implement barebone all dbt models dict building

--- dbt_sugar/core/task/bootstrap.py
+++ dbt_sugar/core/task/bootstrap.py
@@ -1,7 +1,10 @@
 """Bootstrap module. Generates placeholders for all models in a dbt project."""
 
 
+import os
+import re
 from pathlib import Path
+from typing import Mapping, Union
 
 from dbt_sugar.core.config.config import DbtSugarConfig
 from dbt_sugar.core.flags import FlagParser
@@ -21,6 +24,24 @@ class BootstrapTask(BaseTask):
         # we specifically run the super init because we need to populate the cache
         # of all dbt models, where they live etc
         super().__init__(flags, dbt_path, sugar_config)
+
+    def build_all_models_dict(self) -> Mapping[str, Mapping[str, Union[str, Path]]]:
+        """Walk through all .sql files and load their info (name, path etc) into a dict."""
+        models = {}
+        for root, _, files in os.walk(self.repository_path):
+            print(root)
+            print(files)
+            if not re.search(self._excluded_folders_from_search_pattern, root):
+                models.update(
+                    {
+                        f.replace(".sql", ""): {"path": Path(root, f)}
+                        for f in files
+                        if f.lower().endswith(".sql")
+                        and f.lower().replace(".sql", "")
+                        not in self._sugar_config.dbt_project_info.get("excluded_models", [])
+                    }
+                )
+        return models
 
     def run(self) -> int:
         # collect all models in the dbt project

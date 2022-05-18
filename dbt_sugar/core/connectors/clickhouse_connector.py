204718a8efe512a94b6c4c6da07cca853e75844d
Paul-Matthieu Riolacci <paul-matthieu.riolacci@withings.com>
2022-05-18 14:31:10 +0200

    fix: add the actual connector

---
+++ dbt_sugar/core/connectors/clickhouse_connector.py
@@ -0,0 +1,39 @@
+"""
+Module Clickhouse connector.
+
+Module dependent of the base connector.
+"""
+from typing import Dict
+
+import sqlalchemy
+
+from dbt_sugar.core.connectors.base import BaseConnector
+
+
+class ClickhouseConnector(BaseConnector):
+    """
+    Connection class for Clickhouse.
+
+    Child class of base connector.
+    """
+
+    def __init__(
+        self,
+        connection_params: Dict[str, str],
+    ) -> None:
+        """
+        Creates the URL and the Engine for future connections.
+
+        Args:
+            connection_params (Dict[str, str]): Dict containing database connection
+                parameters and credentials.
+        """
+        self.connection_url = sqlalchemy.engine.url.URL(
+            drivername="clickhouse+native",
+            host=connection_params.get("host", str()),
+            username=connection_params.get("user", str()),
+            password=connection_params.get("password", str()),
+            database=connection_params.get("database", str()),
+            port=connection_params.get("port", str()),
+        )
+        self.engine = sqlalchemy.create_engine(self.connection_url)

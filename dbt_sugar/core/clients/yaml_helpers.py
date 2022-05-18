09c4d2efe5dd4ea8615ee437eaa0b49aa7e90720
Paul-Matthieu Riolacci <paul-matthieu.riolacci@withings.com>
2022-05-18 14:24:29 +0200

    feat: add a parser for custom schemas in yaml_helpers

--- dbt_sugar/core/clients/yaml_helpers.py
+++ dbt_sugar/core/clients/yaml_helpers.py
@@ -6,6 +6,7 @@ from typing import Any, Dict
 import ruamel.yaml
 import yaml
 import yamlloader
+from collections import OrderedDict
 
 from dbt_sugar.core.exceptions import YAMLFileEmptyError
 from dbt_sugar.core.logger import GLOBAL_LOGGER as logger
@@ -55,3 +56,31 @@ def save_yaml(path: Path, data: Dict[str, Any], preserve_yaml_order: bool = Fals
                 ryaml.dump(data, outfile)
             else:
                 yaml.dump(data, outfile, width=100, Dumper=yamlloader.ordereddict.CDumper)
+
+
+def parse_custom_schemas(path: Path, dbt_project_yaml: str):
+
+    def get_custom_schema(project_dict, parent_path='models'):
+        models = []
+        if isinstance(project_dict, dict):
+            custom_schema = project_dict.pop('+schema', None)
+            if custom_schema:
+                models.append((parent_path, custom_schema))
+            for potential_model, potential_model_config in project_dict.items():
+                if parent_path:
+                    child_path = f'{parent_path}/{potential_model}'
+                else:
+                    child_path = potential_model
+                res = get_custom_schema(potential_model_config, child_path)
+                if len(res):
+                    models.extend(res)
+        models.sort(key=lambda x: len(x[0]), reverse=True)
+        return models
+
+    dbt_project_dict = open_yaml(path.joinpath(Path(dbt_project_yaml)))
+    dbt_models = dbt_project_dict.get('models')
+    if dbt_models:
+        dbt_project_name = next(reversed(dbt_models))
+
+    ordered_result = get_custom_schema(dbt_project_dict['models'][dbt_project_name])
+    return OrderedDict(ordered_result)
